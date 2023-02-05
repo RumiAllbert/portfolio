b7aa54dc8a1ce590bd1e0b5c197166d03be4ba20
Rumi Allbert <rumiallbert@gmail.com>
2023-02-05 15:30:18 -0500

    [update] skills section

--- portfolio.js
+++ portfolio.js
@@ -22,87 +22,88 @@ export const socialLinks = {
 };
 
 export const skillsSection = {
-	title: "What I do",
-	subTitle:
-		"Passionate data science student who enjoys solving problems and identifying patterns",
-	skills: [
-		emoji(
-			"⚡ Analyze data for trends and patterns to answer questions"
-		),
-		emoji(
-			"⚡ Undertake preprocessing of structured and unstructured data"
-		),
-		emoji(
-			"⚡ Create dashboards and data-driven reports using present-day techniques in graphical analysis "
-		),
-	],
+  title: "Expertise",
+  subTitle: "Data Scientist & ML Engineer",
+  skills: [
+    emoji("📈 Data exploration, cleaning, and analysis"),
+    emoji("🔍 Statistical analysis, hypothesis testing, and modeling"),
+    emoji("📊 Creation and deployment of data-driven models"),
+    emoji("👷🏽 Development and maintenance of data pipelines"),
+    emoji("🔮 Deep learning techniques and applications"),
+    emoji("💻 Big data tools (Spark, Hadoop)"),
+  ],
 
-	softwareSkills: [
-		{
-			skillName: "Python",
-			fontAwesomeClassname: "logos:python",
-		},
-		{
-			skillName: "MySQL",
-			fontAwesomeClassname: "vscode-icons:file-type-mysql",
-		},
-		{
-			skillName: "Rstudio",
-			fontAwesomeClassname: "vscode-icons:file-type-r",
-		},
-		{
-			skillName: "mongoDB",
-			fontAwesomeClassname: "vscode-icons:file-type-mongo",
-		},
-		{
-			skillName: "Tableau",
-			fontAwesomeClassname: "logos:tableau",
-		},
-		{
-			skillName: "Excel",
-			fontAwesomeClassname: "vscode-icons:file-type-excel",
-		},
-		{
-			skillName: "AWS",
-			fontAwesomeClassname: "logos:aws",
-		},
+  softwareSkills: [
+    {
+      skillName: "Python",
+      fontAwesomeClassname: "logos:python",
+    },
+    {
+      skillName: "MySQL",
+      fontAwesomeClassname: "vscode-icons:file-type-mysql",
+    },
+    {
+      skillName: "Go",
+      //   fontAwesomeClassname: "vscode-icons:file-type-golang",
+      fontAwesomeClassname: "vscode-icons:file-type-go",
+    },
+    {
+      skillName: "Rstudio",
+      fontAwesomeClassname: "vscode-icons:file-type-r",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "Tableau",
+      fontAwesomeClassname: "logos:tableau",
+    },
+    {
+      skillName: "Excel",
+      fontAwesomeClassname: "vscode-icons:file-type-excel",
+    },
+    {
+      skillName: "AWS",
+      fontAwesomeClassname: "logos:aws",
+    },
 
-		{
-			skillName: "Git",
-			fontAwesomeClassname: "logos:git-icon",
-		},
-		{
-			skillName: "Docker",
-			fontAwesomeClassname: "logos:docker-icon",
-		},
-				{
-			skillName: "html-5",
-			fontAwesomeClassname: "vscode-icons:file-type-html",
-		},
-		{
-			skillName: "css3",
-			fontAwesomeClassname: "vscode-icons:file-type-css",
-		},
-		{
-			skillName: "JavaScript",
-			fontAwesomeClassname: "logos:javascript",
-		},
-	],
+    {
+      skillName: "Git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+    {
+      skillName: "Docker",
+      fontAwesomeClassname: "logos:docker-icon",
+    },
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+  ],
 };
 
 export const SkillBars = [
-	{
-		Stack: "Data Analysis", //Insert stack or technology you have experience in
-		progressPercentage: "90", //Insert relative proficiency in percentage
-	},
-	{
-		Stack: "Programming",
-		progressPercentage: "80",
-	},
-	{
-		Stack: "Full Stack",
-		progressPercentage: "60",
-	},
+  {
+    Stack: "Data Analysis", //Insert stack or technology you have experience in
+    progressPercentage: "90", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "80",
+  },
+  {
+    Stack: "Full Stack",
+    progressPercentage: "60",
+  },
 ];
 
 export const educationInfo = [
@@ -126,91 +127,80 @@ export const educationInfo = [
 ];
 
 export const experience = [
-  {
-    role: "Junior Data Scientist",
-    company: "The Epoch Times",
-    companylogo: "/img/icons/common/eet.jpg",
-    date: "Jan 2022 – Present",
-    descBullets: [
-      "Utilized statistical and graphical techniques to interpret and analyze data, providing regular reports to stakeholders and upper management.",
-      "Implemented automated data pipelines using Python to streamline business processes, resulting in a 25% increase in efficiency and decreased data leakage.",
-      "Developed and deployed advanced machine learning algorithms, including Deep Neural Network Natural Language Processing (NLP), to identify patterns and predict outcomes within company data streams and customer feedback, resulting in a 20% reduction in unsubscriptions.",
-    ],
-  },
-  {
-    role: "Research Assistant",
-    company: "University of Delaware",
-    companylogo: "/img/icons/common/University_of_Delaware.svg",
-    date: "Dec 2021 – Present",
-    descBullets: [
-      "Analyze data utilizing Python (Pandas, NumPy) and various statistical methods.",
-      "Write Python scripts to integrate and transform data for further analysis in PsychoPy.",
-      "Conduct research and summarize findings on project findings.",
-    ],
-  },
-  {
-    role: "Data Science Intern",
-    company: "LiteSpeed Technologies",
-    companylogo: "/img/icons/common/litespeed.png",
-    date: "May 2021 – Nov 2021",
-    // desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    descBullets: [
-      "Built data-driven reports for VP & CEO of analysis performed and data mining generated results.",
-      "Optimized data mining scripts 10x using SQL and Python knowledge.",
-      "Deploy statistical models and big data algorithms to various company data streams using Linux shells and Python scripts.",
-      "Structured data queries and data mining through the MongoDB (NoSQL) database using Python, and its Pandas package.",
-      "Write a program to fetch, tidy, and process more than 100 million data points using Python and MySQL.",
-      // "Learned the basics of LAMP stack development to deploy, build, and customize a website.",
-    ],
-  },
-  {
-    role: "Web Developer & Assistant Creative Director",
-    company: "The Werking Title Gallery",
-    companylogo: "/img/icons/common/werkingtitlegallery.png",
-    date: "Feb 2020 – May 2020",
-    // desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    descBullets: [
-      "Designed and developed a user-friendly website. Design included font choice, images, formatting, and layout.",
-      "Worked as a positive and productive member of the gallery to inform and promote creative ideas.",
-      "Fixed bugs and enhanced website functionality.",
-    ],
-  },
-  {
-    role: "NASA a World Bridge Alumni",
-    company: "NASA",
-    companylogo: "/img/icons/common/NASA.svg",
-    date: "Aug 2019 – June 2020",
-    // desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    descBullets: [
-      "Developed foundational understandings of Data Science skills directly applied to solving real projects, including computer programming, statistical analysis, data forecasting, and collaborative project management.",
-      "Created user-friendly data visualizations and publicly accessible USGS geospatial maps under mentorship of NASA, United Nations, United States Geological Survey (USGS), and more.",
-      "Assembled Earthquake Signal Precursors by developing a grounded understanding and hands-on application of electrical systems, circuits, and proficient soldering techniques.",
-    ],
-  },
-  {
-    role: "Graphic Designer & Assistant Creative Director",
-    company: "The AI Organization",
-    companylogo: "/img/icons/common/aiorg.jpeg",
-    date: "Jan 2019 – July 2020",
-    // desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    descBullets: [
-      "Collaborated with internal and external stakeholders to gather and define business requirements, establish scopes and managed project milestones.",
-      "Created images and layouts for over 10 projects utilizing Adobe software, and contributed design ideas in early planning stages with customers and project managers.",
-    ],
-  },
-  //   {
-  //     role: "Business Owner & Executive Chef",
-  //     company: "Local Espacio Común",
-  //     companylogo: "/img/icons/common/local.png",
-  //     date: "Apr 2018 – Jan 2019",
-  //     // desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //     descBullets: [
-  //       "Coordinated analysis and planning of restaurant profitability and sales levels.",
-  //       "Developed recipes and menus by applying understanding of market demand and culinary trends.",
-  //       "Hired, trained and managed all kitchen staff, including employee development, issuing disciplinary action and conducting performance reviews.",
-  //       "Collaborated with vendors to source desired recipe ingredients while maintaining tight cost controls.",
-  //     ],
-  //   },
+	{
+		role: "Research Assistant",
+		company: "University of Delaware",
+		companylogo: "/img/icons/common/University_of_Delaware.svg",
+		date: "Dec 2021 – Present",
+		descBullets: [
+			"Analyze data utilizing Python (Pandas, NumPy) and various statistical methods.",
+			"Write Python scripts to integrate and transform data for further analysis in PsychoPy.",
+			"Conduct research and summarize findings on project findings.",
+		],
+	},
+	{
+		role: "Data Science Intern",
+		company: "LiteSpeed Technologies",
+		companylogo: "/img/icons/common/litespeed.png",
+		date: "May 2021 – Nov 2021",
+		// desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Built data-driven reports for VP & CEO of analysis performed and data mining generated results.",
+			"Optimized data mining scripts 10x using SQL and Python knowledge.",
+			"Deploy statistical models and big data algorithms to various company data streams using Linux shells and Python scripts.",
+			"Structured data queries and data mining through the MongoDB (NoSQL) database using Python, and its Pandas package.",
+			"Write a program to fetch, tidy, and process more than 100 million data points using Python and MySQL.",
+			// "Learned the basics of LAMP stack development to deploy, build, and customize a website.",
+		],
+	},
+	{
+		role: "Web Developer & Assistant Creative Director",
+		company: "The Werking Title Gallery",
+		companylogo: "/img/icons/common/werkingtitlegallery.png",
+		date: "Feb 2020 – May 2020",
+		// desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Designed and developed a user-friendly website. Design included font choice, images, formatting, and layout.",
+			"Worked as a positive and productive member of the gallery to inform and promote creative ideas.",
+			"Fixed bugs and enhanced website functionality.",
+		],
+	},
+	{
+		role: "NASA a World Bridge Alumni",
+		company: "NASA",
+		companylogo: "/img/icons/common/NASA.svg",
+		date: "Aug 2019 – June 2020",
+		// desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Developed foundational understandings of Data Science skills directly applied to solving real projects, including computer programming, statistical analysis, data forecasting, and collaborative project management.",
+			"Created user-friendly data visualizations and publicly accessible USGS geospatial maps under mentorship of NASA, United Nations, United States Geological Survey (USGS), and more.",
+			"Assembled Earthquake Signal Precursors by developing a grounded understanding and hands-on application of electrical systems, circuits, and proficient soldering techniques.",
+		],
+	},
+	{
+		role: "Graphic Designer & Assistant Creative Director",
+		company: "The AI Organization",
+		companylogo: "/img/icons/common/aiorg.jpeg",
+		date: "Jan 2019 – July 2020",
+		// desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Collaborated with internal and external stakeholders to gather and define business requirements, establish scopes and managed project milestones.",
+			"Created images and layouts for over 10 projects utilizing Adobe software, and contributed design ideas in early planning stages with customers and project managers.",
+		],
+	},
+	{
+		role: "Business Owner & Executive Chef",
+		company: "Local Espacio Común",
+		companylogo: "/img/icons/common/local.png",
+		date: "Apr 2018 – Jan 2019",
+		// desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Coordinated analysis and planning of restaurant profitability and sales levels.",
+			"Developed recipes and menus by applying understanding of market demand and culinary trends.",
+			"Hired, trained and managed all kitchen staff, including employee development, issuing disciplinary action and conducting performance reviews.",
+			"Collaborated with vendors to source desired recipe ingredients while maintaining tight cost controls.",
+		],
+	},
 ];
 
 export const projects = [
